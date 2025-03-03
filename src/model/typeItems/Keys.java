30d0d11c9cea9a4f170eff6de6b85f58d5d3b10d
Pablo Barra Urtubia <pbarrau.90@icloud.com>
2025-03-02 21:49:14 -0300

    Se crea keys para representar los item de tipo llave, de debinen atributos y metodos

--- src/model/typeItems/Keys.java
+++ src/model/typeItems/Keys.java
@@ -1,4 +1,20 @@
 package model.typeItems;
 
-public class Keys {
+import model.Inventory;
+import model.Item;
+
+public class Keys extends Item {
+    public Keys() {
+        super("Llave","Llave maestra, con ella podras abrir cualquier tipo de cerraduras. Pero solo una vez!", 1,true,true);
+    }
+
+    @Override
+    public void interact(Inventory inventory) {
+        if(inventory.contains(this)){
+            System.out.println("Usaste la llave maestra para abrir la cerradura. ");
+            inventory.removeObjectInventory(this);
+        } else {
+            System.out.println("No tienes un llave maestra para abrir las cerraduras. Debes buscar una");
+        }
+    }
 }
